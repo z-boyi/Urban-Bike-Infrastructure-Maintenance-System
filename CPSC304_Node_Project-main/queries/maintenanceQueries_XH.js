16f6b4c354cc8c340f90626820e9e28d0ead99b8
Xinghao Huang <xh512@students.cs.ubc.ca>
2026-03-20 13:53:14 -0700

    Complete Join Query

--- CPSC304_Node_Project-main/queries/maintenanceQueries_XH.js
+++ CPSC304_Node_Project-main/queries/maintenanceQueries_XH.js
@@ -1,8 +1,11 @@
-// Insert Query
+// INSERT Query
 async function insertMaintenanceTask(TaskID, MaintenanceID, TechnicianID) {
     return await withOracleDB(async (connection) => {
         const result = await connection.execute(
-            `INSERT INTO MaintenanceTask (TaskID, MaintenanceID, TechnicianID) VALUES(:TaskID, :MaintenanceID, :TechnicianID)`,
+            `
+            INSERT INTO MaintenanceTask (TaskID, MaintenanceID, TechnicianID) 
+            VALUES(:TaskID, :MaintenanceID, :TechnicianID)
+            `,
             { TaskID, MaintenanceID, TechnicianID },
             { autoCommit: true }
         );
@@ -13,5 +16,46 @@ async function insertMaintenanceTask(TaskID, MaintenanceID, TechnicianID) {
             return { success: false, message: "MaintenanceID or TechnicianID does not exist." };
         }
         return { success: false, message: "Insert failed." };
+    });
+}
+
+// JOIN Query
+async function getTasksByTechnicianID(TechnicianID) {
+    return await withOracleDB(async (connection) => {
+
+        // Check if the TechnicianID exists
+        const checkExistence = await connection.execute(
+            `
+            SELECT *
+            FROM Technician T
+            WHERE T.StaffID = TechnicianID
+            `,
+            { TechnicianID } 
+        );
+        if (checkExistence.rows.length === 0) {
+            return { success: false, message: "TechnicianID does not exist."};
+        } 
+
+        // Find tasks with information associate with this technician
+        const result = await connection.execute(
+            `
+            SELECT T.StaffID, MT.TaskID, M.MaintenanceID, M.PriorityLevel, M.CompletionStatus, M.RepairQuotation
+            FROM Technician T
+            JOIN MaintenanceTask MT 
+                ON T.StaffID = MT.TechnicianID
+            JOIN Maintenance M 
+                ON MT.TaskID = M.TaskID
+            WHERE StaffID = :TechnicianID
+            `,
+            { TechnicianID },
+        );
+        if (result.rows.length === 0) {
+            return { success: true, message: "Technician exists but has no tasks."}
+        }
+        return {success: true, data: result.rows};
+    }).catch(() => {
+        if (error.errorNum === 2291) {
+            return { success: false, message: "Query failed" };
+        }
     });
 }
