f7a8a88dd761deb3669004392073a4fda6c6605d
Boyi Zhang <bzhang54@students.cs.ubc.ca>
2026-03-22 08:58:48 -0700

    Complete Update Endpoint

---
+++ CPSC304_Node_Project-main/endpoints/endpoints_BZ.js
@@ -0,0 +1,24 @@
+// UPDATE ENDPOINT
+router.post("/update-bike-status", async (req, res) => {
+    const { bikeID, newStatus } = req.body;
+
+    const result = await updateBikeStatus(bikeID, newStatus);
+
+    if (result.success && result.rowsAffected > 0) {
+        res.json({
+            success: true,
+            message: "Bike status updated successfully."
+        });
+    } else if (result.success && result.rowsAffected === 0) {
+        res.json({
+            success: false,
+            message: "No bike found with that BikeID."
+        });
+    } else {
+        res.status(500).json({
+            success: false,
+            message: result.error,
+            details: result.details
+        });
+    }
+});
